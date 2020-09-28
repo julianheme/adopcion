57c913e59e6e025c0be22ac403b2b2a8cc7648b0
SocksHead <juancapoar@gmail.com>
2020-09-28 01:04:18 -0500

    navbar update

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./navbar.module.css";
+import dog from "../../imagenes/ZeriDog.png";
 
 export default function NavBar(props) {
 	return (
 		<nav className={styles.navbar}>
 			<ul className={styles.navList}>
+				<li className={styles.navLogo}>
+					<img src={dog} className={styles.logoImg} />
+				</li>
 				<li className={styles.navItem}>
 					<Link to="/">Inicio</Link>
 				</li>
