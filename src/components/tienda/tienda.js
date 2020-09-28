e6476658ab995fc4617f91c51439a71aea5f47c6
SocksHead <juancapoar@gmail.com>
2020-09-28 01:31:00 -0500

    colors

--- src/components/tienda/tienda.js
+++ src/components/tienda/tienda.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Tienda(props) {
 	return (
 		<div className={`${styles.tarjeta} ${styles.hvrGrow}`}>
-			<Link to={`/tiendas/${props.id}`} style={{ textDecoration: "none", color: "#5b2018" }}>
+			<Link to={`/tiendas/${props.id}`} className={styles.link}>
 				<img className={styles.img} src={props.imagen} />
 				<div className={styles.txt}>
 					<p>{props.nombre}</p>
